c47e5abbc8279d548c7b8bd68f047745f31504e3
Ashok Sasitharan <asasitharan28@gmail.com>
2021-04-08 12:35:08 -0400

    switched to mongo db atlas

---
+++ .history/Server/Config/db_20210408123018.ts
@@ -0,0 +1,9 @@
+export const LocalURI = "mongodb://localhost/webd6201";
+export const RemoteURI = "mongodb+srv://ashok:<password>@webd6201.zr8pg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const Secret = "someSecret";
+export const ConnectionHost = "localhost";
+
+/* module.exports = {
+    Path: mongoDBPath,
+    Secret: sessionSecret
+}  */
--- .history/Server/Config/db_20210408123127.ts
+++ .history/Server/Config/db_20210408123127.ts
@@ -1,7 +1,7 @@
 export const LocalURI = "mongodb://localhost/webd6201";
 export const RemoteURI = "mongodb+srv://ashok:branderspassword@webd6201.zr8pg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 export const Secret = "someSecret";
-export const Host = "MongoDB Atlas";
+export const ConnectionHost = "localhost";
 
 /* module.exports = {
     Path: mongoDBPath,
